2b78deb7b31622e5efab54f58a8a7402745e67cb
David Larson <webd3vdave@gmail.com>
2018-07-07 16:56:26 -0700

    working, fixed navbar styling, layout correct, sticky top

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,19 @@
-import React, {Component} from 'react';
-import './Navbar.css';
+//sets up the reusable Navbar component
+import React, { Component } from "react";
+import "./Navbar.css";
 
 class Navbar extends Component {
-    render() {
-        return(
-            <nav className="navbar navbar-default navbar-fixed-top">
-            <ul>
-                <li className="item-left"></li>
-                <li className="item-center"></li>
-                <li className="item-right">Current Game Score: {this.props.score}</li>
-            </ul>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-default sticky-top">
+         <ul>
+          <li className="item-left">Test Your Memory</li>
+          <li className="item-center"></li>
+          <li className="item-right">Score: {this.props.score}</li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
